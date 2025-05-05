49a8d385347bace27683a63671edd4b1f780b6cf
Hong-Dao <daon53220@gmail.com>
2025-05-05 15:38:00 +0000

    update config

---
+++ config.py
@@ -0,0 +1,4 @@
+MONGO_URI = "mongodb://Hongdao:Dao123@@localhost:27017/chatbot_db?authSource=admin" 
+DB_NAME = "chatbot_db"
+USER_QUESTIONS_COLLECTION = "qa_collection"
+BOT_RESPONSES_COLLECTION = "bot_responses"
